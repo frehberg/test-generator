beeabbd86afd5d27a58138909a0b048d2d1600e1
Sven Neumann <sven@svenfoo.org>
2020-10-15 23:24:47 +0200

    Extend syntax to allow ignore annotation on tests

--- example/build.rs
+++ example/build.rs
@@ -9,8 +9,8 @@ extern crate build_deps;
 fn main() {
     // Enumerate files in sub-folder "res/*", being relevant for the test-generation (as example)
     // If function returns with error, exit with error message.
-    build_deps::rerun_if_changed_paths( "res/*/*" ).unwrap();
+    build_deps::rerun_if_changed_paths("res/*/*").unwrap();
 
     // Adding the parent directory "res" to the watch-list will capture new-files being added
-    build_deps::rerun_if_changed_paths( "res/*" ).unwrap();
+    build_deps::rerun_if_changed_paths("res/*").unwrap();
 }
--- test-generator-utest/src/lib.rs
+++ test-generator-utest/src/lib.rs
@@ -97,11 +97,9 @@
 #[macro_export]
 macro_rules! utest {
     ( $id: ident, $setup:expr, $test:expr, $teardown:expr ) => {
-       #[test]
-       fn $id() {
-            let context = std::panic::catch_unwind(|| {
-                $setup()
-            });
+        #[test]
+        fn $id() {
+            let context = std::panic::catch_unwind(|| $setup());
 
             assert!(context.is_ok());
 
@@ -111,17 +109,13 @@ macro_rules! utest {
                 Err(_) => unreachable!(),
             };
 
-            let result = std::panic::catch_unwind(|| {
-                $test(&ctx)
-            });
+            let result = std::panic::catch_unwind(|| $test(&ctx));
 
-            let finalizer = std::panic::catch_unwind(|| {
-                $teardown(ctx)
-            });
+            let finalizer = std::panic::catch_unwind(|| $teardown(ctx));
 
             assert!(result.is_ok());
 
             assert!(finalizer.is_ok());
-       }
+        }
     };
 }
